ee06ad91b374533356b1cac4bd8b207af700637e
earthsaf <154933783+earthsaf@users.noreply.github.com>
2024-08-25 00:39:40 +0100

    Add files via upload

---
+++ all/database/teksjpm.js
@@ -0,0 +1,32 @@
+*All Transaksi Open ✅*
+
+*List Market - 🛒*
+* Panel Run Bot WhatsApp
+* Nokos WhatsApp (+62)
+* Jasa Unban WhatsApp
+* Jasa Tambah/Edit Fitur SC Bot
+* Jasa Fix Error SC Bot
+* Jasa Ubah SC Bot Scan To Pairing Code
+* Sc Bug Ganas V1.0
+* Sc Bug Qio V13 No Enc Ori
+* Sc Bug Qio V13 Enc Ori
+* Sc Bot Campuran (Button)
+* Sc JagaGrup (antilinkgc)
+* DLL Tanyakan Saja
+
+*List Harga Panel Pterodactyl 🚀*
+* Ram 1GB : Rp1000
+* Ram 2GB : Rp2000
+* Ram 3GB : Rp3000
+* Ram 4GB : Rp4000
+* Ram 5GB : Rp5000
+* Ram 6GB : Rp6000
+* Ram 7GB : Rp7000
+* Ram 8GB : Rp8000
+* Ram 9GB : Rp9000
+* Ram Unlimited : Rp10.000
+*Bergaransi & Server Private*
+
+*Minat ? Hubungi :*
+* 🪀 WhatsApp
+https://wa.me/6285603293224
